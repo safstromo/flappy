d3f1a9606cf5df813f329c5d609a86e817fcb68b
Oliver <safstrom.oliver@gmail.com>
2023-02-27 11:16:00 +0100

    Add Scoring

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::fmt::format;
+
 use bracket_lib::prelude::*;
 
 struct State {
     mode: GameMode,
     frame_time: f32,
     player: Player,
+    obstacle: Obstacle,
+    score: i32,
 }
 impl State {
     fn new() -> Self {
@@ -11,21 +15,32 @@ impl State {
             mode: GameMode::Menu,
             frame_time: 0.0,
             player: Player::new(5, 25),
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0,
         }
     }
     fn play(&mut self, ctx: &mut BTerm) {
+        ctx.print(0, 0, "Press space to flap");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
         ctx.cls_bg(NAVY);
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
         self.frame_time += ctx.frame_time_ms;
         if self.frame_time > FRAME_DURATION {
             self.frame_time = 0.0;
             self.player.gravity_and_move();
         }
+
         if let Some(VirtualKeyCode::Space) = ctx.key {
             self.player.flap();
         }
+
         self.player.render(ctx);
-        ctx.print(0, 0, "Press space to flap");
-        if self.player.y > SCREEN_HEIGHT {
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -33,6 +48,8 @@ impl State {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
         self.mode = GameMode::Playing;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.score = 0;
     }
     fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
@@ -50,6 +67,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play again");
         ctx.print_centered(9, "(Q) Quit game");
         if let Some(key) = ctx.key {
