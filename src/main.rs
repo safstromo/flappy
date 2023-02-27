2499b6ea7599c12e396eae9bb4386eba5bd41968
Oliver <safstrom.oliver@gmail.com>
2023-02-27 10:08:00 +0100

    Add gravity_and_move

--- src/main.rs
+++ src/main.rs
@@ -73,8 +73,18 @@ impl Player {
             velocity: 0.0,
         }
     }
-    fn render (&mut self, ctx: &mut BTerm){
+    fn render(&mut self, ctx: &mut BTerm) {
         ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
     }
 }
 
