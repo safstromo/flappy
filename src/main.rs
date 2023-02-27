3c59ac730dcc4daada3f410e673391e17f47fc42
Oliver <safstrom.oliver@gmail.com>
2023-02-27 10:44:21 +0100

    Add Obsticle

--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,22 @@ impl Player {
     }
 }
 
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+}
+
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy dragon")
